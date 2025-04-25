b576a307b2df8543ee584e577491f6b010305609
Sampath Sanjeewa <Sampath.S@inbay.co.uk>
2025-04-26 01:35:02 +0300

    feat: solution for delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const filename = 'fileToRemove.txt';
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filepath = path.join(__dirname, 'files', filename);
+
+    await fs.unlink(filepath);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
