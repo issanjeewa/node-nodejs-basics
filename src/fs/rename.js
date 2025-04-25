d52e4c6f00af01cc4ee45bdb0878a632a850c9fa
Sampath Sanjeewa <Sampath.S@inbay.co.uk>
2025-04-26 01:30:34 +0300

    feat: solution for rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const oldFileName = 'wrongFilename.txt';
+    const newFileName = 'properFilename.md';
+
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const oldPath = path.join(__dirname, 'files', oldFileName);
+    const newPath = path.join(__dirname, 'files', newFileName);
+
+    await fs.rename(oldPath, newPath);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
