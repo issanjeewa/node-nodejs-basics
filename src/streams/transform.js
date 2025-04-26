b2e407f8f6cfe59178c1b5290bbe4cc0473506c2
Sampath Sanjeewa <Sampath.S@inbay.co.uk>
2025-04-26 18:45:49 +0300

    feat: solutions for stream tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  // Write your code here
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const transformedChunk = chunk.toString().split('').reverse().join('');
+      this.push(transformedChunk + '\n');
+      callback();
+    },
+  });
+
+  process.stdin.pipe(transformStream).pipe(process.stdout);
+  // NOTE after running, type something in the terminal and press enter, to exit press ctrl + C
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filepath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+  const writeStream = fs.createWriteStream(filepath);
+
+  process.stdin.pipe(writeStream);
+  // NOTE after running type something in console and press enter, to exit press ctrl + C
 };
 
 await write();
