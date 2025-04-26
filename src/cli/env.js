d0330b9f065e28e0e1b8132210faef4c6d12438d
Sampath Sanjeewa <Sampath.S@inbay.co.uk>
2025-04-26 11:03:42 +0300

    feat: solution for parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const envVars = Object.entries(process.env || []);
+  const envVarsWithRSS = envVars.filter(([key, value]) => {
+    return key.startsWith('RSS_');
+  });
+
+  const envsAsString = envVarsWithRSS
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  console.log(envsAsString);
 };
 
 parseEnv();
