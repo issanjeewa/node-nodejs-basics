efd6f097e19cff9bae485def408d9672fc49e758
Sampath Sanjeewa <Sampath.S@inbay.co.uk>
2025-04-26 19:36:15 +0300

    feat: solutions for the zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import {} from 'stream';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const filename = 'fileToCompress.txt';
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filepath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+  const gzip = zlib.createGzip();
+  gzip.on('error', (err) => console.error(`Error while compression: `, err));
+
+  const sourceFileStream = fs.createReadStream(filepath);
+  sourceFileStream.on('error', (err) =>
+    console.error(`Error while reading file: `, err)
+  );
+
+  const writeFileStream = fs.createWriteStream(
+    path.join(__dirname, 'files', 'archive.gz')
+  );
+  writeFileStream.on('error', (err) =>
+    console.error(`Error while writing file: `, err)
+  );
+
+  await pipeline(sourceFileStream, gzip, writeFileStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const archivePath = path.join(__dirname, 'files', 'archive.gz');
+  const destinationPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+  const gunzip = zlib.createGunzip();
+  gunzip.on('error', (err) =>
+    console.error(`Error while decompression: `, err)
+  );
+
+  const archiveReadStream = fs.createReadStream(archivePath);
+  archiveReadStream.on('error', (err) =>
+    console.error(`Error while reading file: `, err)
+  );
+
+  const writeFileStream = fs.createWriteStream(destinationPath);
+  writeFileStream.on('error', (err) =>
+    console.error(`Error while writing file: `, err)
+  );
+
+  await pipeline(archiveReadStream, gunzip, writeFileStream);
+  // NOTE file will be unzipped and will be replaced if original file exists.
 };
 
 await decompress();
