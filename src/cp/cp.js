77ccd8b574d64b3517a6dea99615ebb3d01e148d
Sampath Sanjeewa <Sampath.S@inbay.co.uk>
2025-04-27 22:49:11 +0300

    feat: solution for the child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  args = args.slice(2);
+
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const scriptPath = path.join(__dirname, 'files', 'script.js');
+
+  const child = spawn('node', [scriptPath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit'],
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(process.argv);
