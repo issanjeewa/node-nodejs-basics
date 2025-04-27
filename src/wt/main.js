dad9e26031c8901561923a7b3a57996fd15d4b3e
Sampath Sanjeewa <Sampath.S@inbay.co.uk>
2025-04-27 16:43:17 +0300

    feat: solution for worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const workerPath = path.join(__dirname, 'worker.js');
+
+  const startNum = 10;
+
+  const numberOfCores = os.cpus()?.length || os.availableParallelism();
+  const results = Array(numberOfCores).fill(null);
+
+  for (let i = 0; i < numberOfCores; i++) {
+    const input = startNum + i;
+    const worker = new Worker(workerPath, { workerData: { num: input } });
+    worker.on('message', (result) => {
+      results[i] = { status: 'resolved', data: result };
+
+      if (!results.includes(null)) {
+        console.log(results);
+      }
+    });
+
+    worker.on('error', (err) => {
+      results[i] = { status: 'error', data: null };
+      console.error(`Error from worker ${i}`, err);
+
+      if (!results.includes(null)) {
+        console.log(results);
+      }
+    });
+  }
 };
 
 await performCalculations();
